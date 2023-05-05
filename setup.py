ce405c08868dcb790367ea9f1f39d13a346dcdfd
Nat Lee <natlee.work@gmail.com>
2023-05-05 15:41:41 +0800

    [update] add pkg assets

---
+++ setup.py
@@ -0,0 +1,51 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='FisheyeWarping',
+    author='Nat Lee',
+    author_email='natlee.work@gmail.com',
+    description='Dewarp top-down fisheye image to panorama and rewarp panorama to fisheye image.',
+    keywords='dewarp, rewarp, panorama, fisheye, mesh, cli',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/NatLee/fisheye-warping',
+    project_urls={
+        'Documentation': 'https://github.com/NatLee/fisheye-warping',
+        'Bug Reports': 'https://github.com/NatLee/fisheye-warping/issues',
+        'Source Code': 'https://github.com/NatLee/fisheye-warping'
+    },
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    classifiers=[
+        # see https://pypi.org/classifiers/
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=['opencv-python', 'numpy', 'tqdm'],
+    extras_require={
+        'dev': ['check-manifest'],
+        # 'test': ['coverage'],
+    },
+    entry_points={
+        'console_scripts': [
+            'fisheyewarping=fisheyewarping.cli:main'
+        ]
+    }
+)
